9c3eaefddcd707258d4acce4ca3f30b384b5bb81
TinoZg <42203467+TinoZg@users.noreply.github.com>
2019-05-10 17:13:29 +0200

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas_');
 const ctx = canvas.getContext('2d');
-
+let xSpeed = 0;
+let ySpeed = 0;
 
 let snake;
 
@@ -12,15 +13,38 @@ function setup() {
 
 function draw() {
   //set background to black
-  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillStyle = 'rgb(0, 0, 0)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   snake.show();
-  snake.move();
+  snake.move(xSpeed, ySpeed);
   requestAnimationFrame(draw);
 }
 
 setup();
 
-document.addEventListener('keypress', (event) => {
-  console.log(event);
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 37) {
+    console.log("LEFT");
+    xSpeed = -1;
+    ySpeed = 0;
+  }
+
+  if (event.keyCode === 38) {
+    console.log("UP");
+    xSpeed = 0;
+    ySpeed = -1;
+  }
+
+  if (event.keyCode === 39) {
+    console.log("RIGHT");
+    xSpeed = 1;
+    ySpeed = 0;
+  }
+
+  if (event.keyCode === 40) {
+    console.log("DOWN");
+    xSpeed = 0;
+    ySpeed = 1;
+  }
+  //console.log(event);
 });
